39b317eaaa20b3c55df56792af93830c8e2f4422
dahong <1010657594@qq.com>
2018-06-19 20:08:02 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -16,9 +17,27 @@ class UsersController extends Controller
     {
         return view('users.edit',compact('user'));
     }
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
-       $user->update($request->all());
+        //我们可直接通过 请求对象（Request） 来获取用户上传的文件 使用的dd的调试方法
+        // 第一种方法
+       //dd($request->file('avatar'));
+        // 第二种方法，可读性更高
+        //$file = $request->avatar;
+        $data = $request->all();
+
+        if($request->avatar){
+            //save第一个参数指的是需要上传的文件信息,第二个文件夹文件名 ,第三个文件的前缀名字
+            $result=$uploader->save($request->avatar,'avatar',$user->id,362);
+            if($request){
+                //将返回的path图片地址存储在avatar
+                $data['avatar'] = $result['path'];
+            }
+        }
+        //dump($data)下数据 得到的是包含了存储地址的"avarar=>http://hong.larabbs.com/uploads/images/avatar/201806/19/3_1529383269_cbAkWOBYY1.png"的数组数据
+
+        //开始更新数据
+       $user->update($data);
        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 }
